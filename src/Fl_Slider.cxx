46bef3eff91f4572c7db919c5046be4048799928
Jonathan Moore Liles <j.liles@unix.net>
2012-03-10 23:48:01 -0800

    Fl_Slider: Since when does 'black' mean the foreground color?

--- src/Fl_Slider.cxx
+++ src/Fl_Slider.cxx
@@ -110,7 +110,7 @@ void Fl_Slider::draw_bg(int X, int Y, int W, int H) {
   draw_box();
   fl_pop_clip();
 
-  Fl_Color black = active_r() ? FL_FOREGROUND_COLOR : FL_INACTIVE_COLOR;
+  Fl_Color black = active_r() ? FL_BLACK : FL_INACTIVE_COLOR;
   if (type() == FL_VERT_NICE_SLIDER) {
     draw_box(FL_THIN_DOWN_BOX, X+W/2-2, Y, 4, H, black);
   } else if (type() == FL_HOR_NICE_SLIDER) {
